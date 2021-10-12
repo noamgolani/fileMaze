d3f5add1286cb9bed1c2376b756ab18f8e795de0
noamgolani <noam.golani@gmail.com>
2021-10-12 15:10:19 +0300

    Sync solution

--- mazeRunner.js
+++ mazeRunner.js
@@ -48,4 +48,51 @@ function openChest(chestPath, callback) {
    });
 }
 
-runMaze("./maze");
+function runMazeSync(root) {
+   const result = readMazeRoomSync(root);
+   console.log(`\x1b[32mFound: ${result}\x1b[0m`);
+}
+
+function readMazeRoomSync(roomPath) {
+   try {
+      const items = fs.readdirSync(roomPath);
+      while (items.length > 0) {
+         const item = items.pop();
+         const itemPath = path.join(roomPath, item);
+         const stat = fs.lstatSync(itemPath);
+         if (stat.isFile()) {
+            try {
+               const chestContent = openChestSync(itemPath);
+               if (chestContent === "found") return itemPath;
+               if (chestContent) {
+                  console.log(`Going to room: ${chestContent}`);
+                  try {
+                     return readMazeRoomSync(chestContent);
+                  } catch (err) {
+                     console.log(`\x1b[31m${err.message}\x1b[0m`);
+                  }
+               }
+            } catch (err) {
+               console.log(`\x1b[31m${err.message}\x1b[0m`);
+            }
+         }
+      }
+   } catch (err) {
+      throw new Error(`Cant find the door to room: ${roomPath}`);
+   }
+}
+
+function openChestSync(chestPath) {
+   try {
+      console.log(`Opening chest: ${chestPath}`);
+      const content = fs.readFileSync(chestPath);
+      const jsonContent = JSON.parse(content);
+      if (jsonContent.treasure) return "found";
+      if (jsonContent.next) return jsonContent.next;
+      return null;
+   } catch (err) {
+      throw new Error(`Cant open chest: ${chestPath}`);
+   }
+}
+
+runMazeSync("./maze");
