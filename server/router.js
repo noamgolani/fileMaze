fe27b06d372c8e1585a905caf954cea5be3d19ee
noamgolani <noam.golani@gmail.com>
2021-10-18 15:38:53 +0300

    server works

--- server/router.js
+++ server/router.js
@@ -22,9 +22,35 @@ async function roomHandler(req, res) {
 
   try {
     const dirData = await fs.readdir(
-      path.join(__dirname, '../maze', query.dir)
+      path.join(__dirname, '../maze', query.dir),
     );
     goodRes(res, dirData);
+  } catch (err) {
+    badRes(res, err);
+  }
+}
+
+async function chestHandler(req, res) {
+  const { query } = req;
+
+  try {
+    const fileData = await fs.readFile(
+      path.join(__dirname, '../maze', query.dir),
+    );
+    goodRes(res, JSON.parse(fileData.toString()));
+  } catch (err) {
+    badRes(res, err);
+  }
+}
+
+async function statHandler(req, res) {
+  const { query } = req;
+
+  try {
+    const statData = await fs.lstat(
+      path.join(__dirname, '../maze', query.dir),
+    );
+    goodRes(res, statData);
   } catch (err) {
     badRes(res, err);
   }
@@ -33,16 +59,20 @@ async function roomHandler(req, res) {
 module.exports.handleRequest = (req, res) => {
   const { method, url, body } = req;
   console.log(
-    `${method} Request to - ${url} \n\t Body: ${JSON.stringify(body)}`
+    `${method} Request to - ${url} \n\t Body: ${JSON.stringify(body)}`,
   );
 
   const withoutQuery = url.split('?')[0];
+  // TODO change to map
   switch (withoutQuery.split('/')[1]) {
     case 'room':
       roomHandler(req, res);
       break;
     case 'chest':
       chestHandler(req, res);
+      break;
+    case 'stat':
+      statHandler(req, res);
       break;
     default:
       res.writeHead(404, 'Page not found', {
