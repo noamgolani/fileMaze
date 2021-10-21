809cfd76c719f253ba35ff77e063f6f09bd439db
noamgolani <noam.golani@gmail.com>
2021-10-21 11:50:26 +0300

    using a logger package

--- src/runner.js
+++ src/runner.js
@@ -1,35 +1,23 @@
 /* eslint-disable consistent-return */
 import axios from 'axios';
-import fs from 'fs/promises';
 import path from 'path';
+import pino from 'pino';
 
 axios.defaults.baseURL = 'http://localhost:5000';
 
-class Logger {
-  constructor(baseDir) {
-    this.baseDir = baseDir;
-    this.errPath = path.join(this.baseDir, 'error.log');
-    this.logPath = path.join(this.baseDir, 'maze.log');
-
-    fs.rm(this.errPath);
-    fs.rm(this.logPath);
-  }
-
-  async log(msg) {
-    await fs.appendFile(`${this.baseDir}/maze.log`, `${msg}\n`);
-  }
-
-  async logError(error) {
-    await fs.appendFile(`${this.baseDir}/error.log`, `${error.message}\n`);
-  }
-}
-
-const logger = new Logger('logs');
+const logger = pino(
+  {
+    prettyPrint: {
+      colorize: true,
+    },
+  },
+  pino.destination('./log.log'),
+);
 
 async function openChest(chestPath) {
   try {
     const response = await axios.get(`/chest?dir=${chestPath}`);
-    logger.log(`Opened chest: ${chestPath}`);
+    logger.info(`Opened chest: ${chestPath}`);
     const { treasure, next } = response.data;
     if (treasure) {
       return { found: true, content: { path: chestPath, content: treasure } };
@@ -42,14 +30,14 @@ async function openChest(chestPath) {
 
 async function readMazeRoom(roomPath) {
   const response = await axios.get(`/room?dir=${roomPath}`);
-  logger.log(`Entered: ${roomPath}`);
+  logger.info(`Entered: ${roomPath}`);
   const files = response.data.map((file) => path.join(roomPath, file));
 
   // Open all the files in the room
   const chestsContent = (await Promise.allSettled(files.map(openChest)))
     .filter((promiseReturn) => {
       if (promiseReturn.status === 'rejected') {
-        logger.logError(promiseReturn.reason);
+        logger.error(promiseReturn.reason);
         return false;
       }
       return true;
@@ -58,22 +46,22 @@ async function readMazeRoom(roomPath) {
 
   const foundContent = chestsContent.filter(({ found }) => found)[0];
   if (foundContent)
-    return logger.log(
+    return logger.info(
       `Found: ${foundContent.content.content} at: ${foundContent.content.path}`,
     );
 
   chestsContent.forEach(({ content: { next } }) => {
-    readMazeRoom(next).catch((err) => logger.logError(err));
+    readMazeRoom(next).catch((err) => logger.error(err));
   });
 }
 
 export default function runMaze() {
   readMazeRoom('/')
     .then(() => {
-      console.log(`Done! your logs are in: ${path.resolve(logger.baseDir)}`);
+      console.log(`Done!`);
     })
     .catch((err) => {
-      logger.logError(err);
-      console.log(`Cant find a treasur! your logs are in: ${logger.baseDir}`);
+      logger.error(err);
+      console.log(`Cant find a treasur!`);
     });
 }
